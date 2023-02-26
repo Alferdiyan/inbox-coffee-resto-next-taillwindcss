ddae5cf30467b5bd6614866250c350d2fecbb733
Alferdiyan <ferdi1505@gmail.com>
2023-02-27 05:24:25 +0700

    responsive Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,23 +1,55 @@
-import Image from "next/image"
-import Navlink from "./Navlink"
+import Image from 'next/image';
+import Navlink from './Navlink';
 
-import {linkList} from "@/utils/link"
+import { linkList } from '@/utils/link';
+import React, { useState } from 'react';
+import ResponsiveNavLink from './ResponsiveNavLink';
 
 const Navbar = () => {
-  return (
-    <div className="relative z-[10] text-white mt-4">
-        <div className="flex justify-between items-center">
-            <div>
-                <Image src="/logo.png" alt="logo" width={70} height={70}/>
-            </div>
-            <ul className="flex gap-4 text-lg">
-                {linkList.map((link) => (
-                    <Navlink key={link.id} href={link.href} title={link.title}/>
-                ))}
-            </ul>
-        </div>
-    </div>
-  )
-}
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+  const [open, setOpen] = useState(false);
 
-export default Navbar
+  return (
+    <div className="relative z-[20] text-white mt-4">
+      {/* Desktop Nav */}
+      <div className="hidden md:flex md:justify-between items-center">
+        {/* logo */}
+        <div>
+          <Image src="/logo.png" alt="logo" width={70} height={70} />
+        </div>
+        {/* Nav Mneu */}
+        <ul className="md:flex gap-4 text-xs md:text-lg">
+          {linkList.map((link) => (
+            <Navlink key={link.id} href={link.href} title={link.title} />
+          ))}
+        </ul>
+      </div>
+      {/* Desktop Nav */}
+
+      {/* Mobile Nav */}
+      <div className="flex justify-between items-center md:hidden mb-10">
+        <div>
+          <Image src="/logo.png" alt="logo" width={50} height={50} />
+        </div>
+        <div>
+          <div onClick={() => setOpen(false)} className={`${open ? 'block' : 'hidden'} bg-transparent absolute w-full h-full inset-0`}></div>
+          <button onClick={() => setOpen((open) => !open)} className="focus:outline-none">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="text-white w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+            </svg>
+          </button>
+          <div className={`${open ? 'block' : 'hidden'} bg-white absolute right-0 mr-10 top-0 mt-4 py-1 rounded-lg w-56 overflow-hidden`}>
+            <ResponsiveNavLink href="#">Home</ResponsiveNavLink>
+            <ResponsiveNavLink href="#">Our menu</ResponsiveNavLink>
+            <ResponsiveNavLink href="#">Dessert</ResponsiveNavLink>
+            <ResponsiveNavLink href="#">About Us</ResponsiveNavLink>
+            <ResponsiveNavLink href="#">Contact</ResponsiveNavLink>
+          </div>
+        </div>
+      </div>
+      {/* Mobile Nav */}
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import Head from 'next/head'
-import Homepage from "../sections/home.jsx"
-import OurMenu from '@/sections/menu.jsx'
-import Dessert from '@/sections/dessert.jsx'
-import About from '@/sections/about.jsx'
-import Footer from '@/sections/footer.jsx'
+import Head from 'next/head';
+import Homepage from '../sections/home.jsx';
+import OurMenu from '@/sections/menu.jsx';
+import Dessert from '@/sections/dessert.jsx';
+import About from '@/sections/about.jsx';
+import Footer from '@/sections/footer.jsx';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Freshcoffee</title>
+        <title>Inbox Coffee Resto | Tempat Adem Haraga Adem</title>
         <meta name="description" content="freshcoffee website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
@@ -20,5 +20,5 @@ export default function Home() {
       <About />
       <Footer />
     </>
-  )
+  );
 }
