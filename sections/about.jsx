2124a11b0e70cc5d58cef5e539d9ec96a66cfd45
Alferdiyan <ferdi1505@gmail.com>
2023-02-27 04:35:45 +0700

    responsive about and delete component card

--- sections/about.jsx
+++ sections/about.jsx
@@ -4,15 +4,19 @@ import Section from '@/components/Section';
 
 const About = () => {
   return (
-    <Section id="about">
+    <div id="about">
       <div className="h-full">
-        <div className="flex md:flex-row ">
-          <div className=" md:w-[60vw] flex justify-center items-center">
-            <Image src="/image/coffee-shop.jpg" alt="coffee-shop" width={350} height={500} className="rounded-xl" />
+        <div className="md:relative flex sm:flex-col ">
+          <div className="hidden md:w-[60vw] md:flex md:py-6 justify-center items-center ">
+            <Image src="/image/coffee-shop.jpg" alt="coffee-shop" width={350} height={400} className="mb-10 rounded-xl" />
           </div>
 
-          <div className="md:w-[40vw]  bg-[#333131] p-4">
+          <div className="md:absolute md:right-0 md:w-[40vw] bg-[#333131] p-4">
             <h1 className="p-3 text-6xl font-bold text-white opacity-10">Inbox Coffee Resto</h1>
+            <div className="flex justify-center">
+              <Image src="/image/coffee-shop.jpg" alt="coffee-shop" width={250} height={250} className="place-content-center md:hidden rounded-xl" />
+            </div>
+
             <RunningText color="#7d7d7d" align="left" />
             <TitlePage title="About us" color="#e8e8e8" align="left" />
             <div className="mt-8 text-[#e8e8e8] text-lg font-regular text-justify">
@@ -24,30 +28,8 @@ const About = () => {
           </div>
         </div>
       </div>
-    </Section>
+    </div>
   );
 };
 
 export default About;
-
-{
-  /* <div className="flex justify-center items-center">
-          <Image src="/image/coffee-shop.jpg" alt="coffee-shop" width={350} height={500} className="rounded-xl" />
-        </div>
-        <div className="absolute top-0 right-0 w-1/2 h-full bg-[#333131] z-0" />
-
-        <div className="z-50 py-8 px-10 relative flex justify-start items-center">
-          <h1 className="absolute top-10 left-10 text-6xl font-bold text-white opacity-10">Inbox Coffee Resto</h1>
-
-          <div>
-            <RunningText color="#7d7d7d" align="left" />
-            <TitlePage title="About us" color="#e8e8e8" align="left" />
-            <div className="mt-8 text-[#e8e8e8] text-lg font-regular text-justify">
-              <p className="opacity-90">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industries standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
-                a type specimen book.
-              </p>
-            </div>
-          </div>
-        </div> */
-}
