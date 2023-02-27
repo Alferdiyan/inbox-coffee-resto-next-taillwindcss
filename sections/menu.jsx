d379367e904033fa7874ae5fc598cd90466f3caf
Alferdiyan <ferdi1505@gmail.com>
2023-02-27 07:09:02 +0700

    resposive card

--- sections/menu.jsx
+++ sections/menu.jsx
@@ -1,26 +1,25 @@
-import {Ourmenu} from "@/constant"
-import Section from "@/components/Section"
-import CoffeeCard from "@/components/CoffeeCard"
-import { RunningText,TitlePage } from "@/components/TypingText"
+import { Ourmenu } from '@/constant';
+import Section from '@/components/Section';
+import CoffeeCard from '@/components/CoffeeCard';
+import { RunningText, TitlePage } from '@/components/TypingText';
 
-import { useState } from "react"
-
+import { useState } from 'react';
 
 const OurMenu = () => {
-  const [active, setActive] = useState("coffee3")
+  const [active, setActive] = useState('coffee3');
 
   return (
     <Section id="menu">
-        <RunningText />
-        <TitlePage title="Our main menus"/>
+      <RunningText />
+      <TitlePage title="Our main menus" />
 
-        <div className="mt-[50px] flex flex-row min-h-[70vh] gap-2">
-            {Ourmenu.map((coffee, index) => (
-                <CoffeeCard key={coffee.id} {...coffee} active={active} handleClick={setActive} />
-            ))}
-        </div>
+      <div className="mt-[50px] flex flex-col md:flex-row min-h-[70vh] md:min-h-[70vh] gap-2">
+        {Ourmenu.map((coffee, index) => (
+          <CoffeeCard key={coffee.id} {...coffee} active={active} handleClick={setActive} />
+        ))}
+      </div>
     </Section>
-  )
-}
+  );
+};
 
-export default OurMenu
+export default OurMenu;
