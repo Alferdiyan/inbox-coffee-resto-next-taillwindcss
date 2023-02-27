3bd7208183b2186c2bc56fcc523f602360f60e71
Alferdiyan <ferdi1505@gmail.com>
2023-02-27 08:00:42 +0700

    responsive Dessert page

--- sections/menu.jsx
+++ sections/menu.jsx
@@ -9,16 +9,21 @@ const OurMenu = () => {
   const [active, setActive] = useState('coffee3');
 
   return (
-    <Section id="menu">
+    <div id="menu" className="min-h-screen w-screen overflow-x-auto px-10 relative md:px-[8rem] py-[1rem]">
       <RunningText />
       <TitlePage title="Our main menus" />
 
-      <div className="mt-[50px] flex flex-col md:flex-row min-h-[70vh] md:min-h-[70vh] gap-2">
+      <div className="hidden mt-[50px]  md:flex flex-col md:flex-row min-h-[70vh] md:min-h-[70vh] gap-2">
         {Ourmenu.map((coffee, index) => (
           <CoffeeCard key={coffee.id} {...coffee} active={active} handleClick={setActive} />
         ))}
       </div>
-    </Section>
+      <div className="flex flex-row md:hidden gap-2 min-h-[90vh]">
+        {Ourmenu.map((coffee, index) => (
+          <CoffeeCard key={coffee.id} {...coffee} active={active} handleClick={setActive} />
+        ))}
+      </div>
+    </div>
   );
 };
 
