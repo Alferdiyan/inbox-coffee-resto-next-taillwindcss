b99617ae8d81dea7e3c2646d559a58989d11c365
Alferdiyan <ferdi1505@gmail.com>
2023-02-28 09:18:40 +0700

    scrolling Menu

--- sections/menu.jsx
+++ sections/menu.jsx
@@ -9,16 +9,16 @@ const OurMenu = () => {
   const [active, setActive] = useState('coffee3');
 
   return (
-    <div id="menu" className="min-h-screen w-screen overflow-x-auto px-10 relative md:px-[8rem] py-[1rem]">
+    <div id="menu" className="min-h-screen w-screen px-10 relative md:px-[8rem] py-[1rem]">
       <RunningText />
       <TitlePage title="Our main menus" />
 
-      <div className="hidden mt-[50px]  md:flex flex-col md:flex-row min-h-[70vh] md:min-h-[70vh] gap-2">
+      <div className="hidden mt-[50px] overflow-x-auto   md:flex flex-col md:flex-row min-h-[70vh] md:min-h-[70vh] gap-2">
         {Ourmenu.map((coffee, index) => (
           <CoffeeCard key={coffee.id} {...coffee} active={active} handleClick={setActive} />
         ))}
       </div>
-      <div className="flex flex-row md:hidden gap-2 min-h-[90vh]">
+      <div className="flex flex-row md:hidden gap-2 overflow-x-auto  min-h-[90vh]">
         {Ourmenu.map((coffee, index) => (
           <CoffeeCard key={coffee.id} {...coffee} active={active} handleClick={setActive} />
         ))}
