42bb21611c2caf71251f68bb2a7a3b6cdbbe26ee
Alferdiyan <ferdi1505@gmail.com>
2023-02-28 08:23:27 +0700

    bhange name content

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     <>
       <Head>
         <title>Inbox Coffee Resto | Tempat Adem Haraga Adem</title>
-        <meta name="description" content="freshcoffee website" />
+        <meta name="description" content="inbox Coffee & Resto" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
