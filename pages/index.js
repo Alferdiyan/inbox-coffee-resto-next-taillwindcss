7221741f2b84b7522aa51ff77aa84f9e96b592b6
Alferdiyan <ferdi1505@gmail.com>
2023-02-28 08:34:31 +0700

    change logo

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <title>Inbox Coffee Resto | Tempat Adem Haraga Adem</title>
         <meta name="description" content="inbox Coffee & Resto" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo1.svg" />
       </Head>
       <Homepage />
       <OurMenu />
